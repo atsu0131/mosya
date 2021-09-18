cd682f787d6d4685d49e0c5ea7db3db0c8234e44
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-09-18 20:51:32 +0900

    add

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 $(function () {
   $(".burger-btn").on("click", function () {
-    // $(".burger-btn").toggleClass("close");
-    // $(".bar").toggleClass("white");
-    // $(".nav-wrapper").fadeToggle(500);
+    $(".burger-btn").toggleClass("close");
+    $(".bar").toggleClass("white");
+    $(".nav-wrapper").fadeToggle(500);
   });
 });
 
