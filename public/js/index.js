6392e1c6719b90ae91f3e8cf8d470302d0021496
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-09-19 23:58:57 +0900

    add

--- public/js/index.js
+++ public/js/index.js
@@ -7,119 +7,169 @@ $(function () {
 });
 
 $(function () {
-  $(".hover-title1").hover(
-    function () {
+  $(".hover-title1").on({
+    mouseenter: function () {
       $(".menuSub:not(:animated)", this).slideDown();
       $(".js-1").toggleClass("on");
     },
-    function () {
+    mouseleave: function () {
       $(".menuSub", this).slideUp();
-    }
-  );
-  $(".hover-title2").hover(
-    function () {
+      $(".js-1").toggleClass("on");
+    },
+  });
+  $(".hover-title2").on({
+    mouseenter: function () {
       $(".menuSub:not(:animated)", this).slideDown();
       $(".js-2").toggleClass("on");
     },
-    function () {
+    mouseleave: function () {
       $(".menuSub", this).slideUp();
-    }
-  );
-  $(".hover-title4").hover(
-    function () {
-      $(".menuSub:not(:animated)", this).slideDown();
-      $(".js-3").toggleClass("on");
+      $(".js-2").toggleClass("on");
     },
-    function () {
+  });
+  $(".hover-title4").on({
+    mouseenter: function () {
+      $(".menuSub:not(:animated)", this).slideDown();
+      $(".js-4").toggleClass("on");
+    },
+    mouseleave: function () {
       $(".menuSub", this).slideUp();
-    }
-  );
-  $(".hover-title5").hover(
-    function () {
+      $(".js-4").toggleClass("on");
+    },
+  });
+  $(".hover-title5").on({
+    mouseenter: function () {
       $(".menuSub:not(:animated)", this).slideDown();
       $(".js-5").toggleClass("on");
     },
-    function () {
+    mouseleave: function () {
       $(".menuSub", this).slideUp();
-    }
-  );
-  $(".hover-title6").hover(
-    function () {
+      $(".js-5").toggleClass("on");
+    },
+  });
+  $(".hover-title6").on({
+    mouseenter: function () {
       $(".menuSub:not(:animated)", this).slideDown();
       $(".js-6").toggleClass("on");
     },
-    function () {
+    mouseleave: function () {
       $(".menuSub", this).slideUp();
-    }
-  );
+      $(".js-6").toggleClass("on");
+    },
+  });
+  // $(".hover-title1").hover(
+  //   function () {
+  //     $(".menuSub:not(:animated)", this).slideDown();
+  //     $(".js-1").toggleClass("on");
+  //   },
+  //   function () {
+  //     $(".menuSub", this).slideUp();
+  //   }
+  // );
+  // $(".hover-title2").hover(
+  //   function () {
+  //     $(".menuSub:not(:animated)", this).slideDown();
+  //     $(".js-2").toggleClass("on");
+  //   },
+  //   function () {
+  //     $(".menuSub", this).slideUp();
+  //   }
+  // );
+  // $(".hover-title4").hover(
+  //   function () {
+  //     $(".menuSub:not(:animated)", this).slideDown();
+  //     $(".js-4").toggleClass("on");
+  //   },
+  //   function () {
+  //     $(".menuSub", this).slideUp();
+  //   }
+  // );
+  // $(".hover-title5").hover(
+  //   function () {
+  //     $(".menuSub:not(:animated)", this).slideDown();
+  //     $(".js-5").toggleClass("on");
+  //   },
+  //   function () {
+  //     $(".menuSub", this).slideUp();
+  //   }
+  // );
+  // $(".hover-title6").hover(
+  //   function () {
+  //     $(".menuSub:not(:animated)", this).slideDown();
+  //     $(".js-6").toggleClass("on");
+  //   },
+  //   function () {
+  //     $(".menuSub", this).slideUp();
+  //   }
+  // );
 
   $(".burger-btn").click(function () {
     $("#nav").toggleClass("none");
   });
 
-  $(function () {
-    // アイコンを囲うaタグの指定
-    var toggles = $(".js-1");
-    //アイコンをクリックでクラスの切替え
-    toggles.click(function (event) {
-      event.preventDefault();
-      toggles.toggleClass("on");
-      if (toggles.hasClass("on")) {
-      } else {
-        $(".menuSub").slideUp();
-      }
-    });
-  });
-  $(function () {
-    // アイコンを囲うaタグの指定
-    var toggles = $(".js-2");
-    //アイコンをクリックでクラスの切替え
-    toggles.click(function (event) {
-      event.preventDefault();
-      toggles.toggleClass("on");
-      if (toggles.hasClass("on")) {
-      } else {
-        $(".menuSub").slideUp();
-      }
-    });
-  });
-  $(function () {
-    // アイコンを囲うaタグの指定
-    var toggles = $(".js-3");
-    //アイコンをクリックでクラスの切替え
-    toggles.click(function (event) {
-      event.preventDefault();
-      toggles.toggleClass("on");
-      if (toggles.hasClass("on")) {
-      } else {
-        $(".menuSub").slideUp();
-      }
-    });
-  });
-  $(function () {
-    // アイコンを囲うaタグの指定
-    var toggles = $(".js-4");
-    //アイコンをクリックでクラスの切替え
-    toggles.click(function (event) {
-      event.preventDefault();
-      toggles.toggleClass("on");
-      if (toggles.hasClass("on")) {
-      } else {
-        $(".menuSub").slideUp();
-      }
-    });
-  });
-  $(function () {
-    // アイコンを囲うaタグの指定
-    var toggles = $(".js-5");
-    //アイコンをクリックでクラスの切替え
-    toggles.click(function (event) {
-      event.preventDefault();
-      toggles.toggleClass("on");
-      if (toggles.hasClass("on")) {
-      } else {
-        $(".menuSub").slideUp();
-      }
-    });
-  });
+  // $(function () {
+  //   // アイコンを囲うaタグの指定
+  //   var toggles = $(".js-1");
+  //   //アイコンをクリックでクラスの切替え
+  //   toggles.click(function (event) {
+  //     event.preventDefault();
+  //     toggles.toggleClass("on");
+  //     if (toggles.hasClass("on")) {
+  //     } else {
+  //       $(".menuSub").slideUp();
+  //     }
+  //   });
+  // });
+  // $(function () {
+  //   // アイコンを囲うaタグの指定
+  //   var toggles = $(".js-2");
+  //   //アイコンをクリックでクラスの切替え
+  //   toggles.click(function (event) {
+  //     event.preventDefault();
+  //     toggles.toggleClass("on");
+  //     if (toggles.hasClass("on")) {
+  //     } else {
+  //       $(".menuSub").slideUp();
+  //     }
+  //   });
+  // });
+  // $(function () {
+  //   // アイコンを囲うaタグの指定
+  //   var toggles = $(".js-3");
+  //   //アイコンをクリックでクラスの切替え
+  //   toggles.click(function (event) {
+  //     event.preventDefault();
+  //     toggles.toggleClass("on");
+  //     if (toggles.hasClass("on")) {
+  //     } else {
+  //       $(".menuSub").slideUp();
+  //     }
+  //   });
+  // });
+  // $(function () {
+  //   // アイコンを囲うaタグの指定
+  //   var toggles = $(".js-4");
+  //   //アイコンをクリックでクラスの切替え
+  //   toggles.click(function (event) {
+  //     event.preventDefault();
+  //     toggles.toggleClass("on");
+  //     if (toggles.hasClass("on")) {
+  //     } else {
+  //       $(".menuSub").slideUp();
+  //     }
+  //   });
+  // });
+  // $(function () {
+  //   // アイコンを囲うaタグの指定
+  //   var toggles = $(".js-5");
+  //   //アイコンをクリックでクラスの切替え
+  //   toggles.click(function (event) {
+  //     event.preventDefault();
+  //     toggles.toggleClass("on");
+  //     if (toggles.hasClass("on")) {
+  //     } else {
+  //       $(".menuSub").slideUp();
+  //     }
+  //   });
+  // });
 });
